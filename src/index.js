71d96cece2701d50fae2fecf82cf0c6817d925f7
dmitry-sh <matemV@mail.ru>
2020-10-07 17:42:37 +0300

    feat: add reverse code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = n * -1;
+    }
+      let b = String(n);
+      let c = b.split('');
+      let d = c.reverse();
+      let e = d.join('');
+      let i = 0;
+      let f = '';
+      while (i < String(e).length) {
+        if (e[0] == 0) {
+          e = e.substr(1);
+        }
+        i++;
+      }
+      Number(e);
+      return e;
 }
