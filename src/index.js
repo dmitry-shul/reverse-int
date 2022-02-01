cf7232391692338d369fd3e8cce75b2bd8371d41
Dmitry Shulzhytski <matemV@mail.ru>
2022-02-01 21:18:10 +0300

    change solution

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 module.exports = function reverse (n) {
-    if (n < 0) {
-        n = n * -1;
-    }
-      let b = String(n);
-      let c = b.split('');
-      let d = c.reverse();
-      let e = d.join('');
-      let i = 0;
-      let f = '';
-      while (i < String(e).length) {
-        if (e[0] == 0) {
-          e = e.substr(1);
-        }
-        i++;
-      }
-      Number(e);
-      return e;
+    n < 0 ? n *= -1 : n;
+    n[n.length - 1] == 0 ? n.slice(0, n.length - 2) : n;
+    return n.toString().split("").reverse().join("");
 }
